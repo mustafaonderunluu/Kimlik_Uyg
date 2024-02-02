96272fc202d8eaaf37ed0883dd5242089e3d24f1
Mustafa Önder ÜNLÜ <116373504+mustafaonderunluu@users.noreply.github.com>
2024-02-02 15:22:45 +0300

    Proje dosyası ekle.

---
+++ Kimlik_Uyg/Kimlik.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kimlik_Uyg
+{
+    internal class Kimlik
+    {
+        string ad;
+        string soyad;
+        string memleket;
+        int yas;
+        char cinsiyet;
+
+        public string AD { 
+        
+        get { return ad; }set { ad = value.ToLower(); }
+        
+        }
+       public  string SOYAD { 
+        
+        get { return soyad; }set { soyad = value.ToUpper(); }
+        }
+       public string MEMLEKET { 
+        
+            get { return memleket; }set { memleket = value ; }
+        
+        }
+        public int YAS {
+
+            get { return yas; }set { yas = Math.Abs(value); }
+        }
+        public char CINSIYET {
+
+
+            get { return cinsiyet; }set { cinsiyet = value; }
+        }
+        public Kimlik()
+        {
+
+            ad = " ";
+            soyad = " ";
+            memleket = "Ankara ";
+            yas = 18;
+            cinsiyet = 'K';
+
+        }
+
+    }
+}
---
+++ Kimlik_Uyg/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kimlik_Uyg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Kimlik kml = new Kimlik();
+
+            kml.AD = "Mustafa Önder ";
+            kml.SOYAD = "Ünlü";
+            kml.MEMLEKET = "İstanbul ";
+
+            Console.WriteLine(kml.AD);
+            Console.WriteLine(kml.SOYAD);
+            Console.WriteLine(kml.MEMLEKET);
+            Console.WriteLine(kml.YAS);
+            Console.WriteLine(kml.CINSIYET);
+            Console.ReadKey();  
+        }
+    }
+}
